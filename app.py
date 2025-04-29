cf55af75e7829f1952ab8444c5d81bf725939cac
Ryan Tait <ryan.tait31@outlook.com>
2025-04-29 21:48:14 +1000

    internet

--- app.py
+++ app.py
@@ -40,4 +40,4 @@ def get_config(category, filename):
     return send_from_directory(folder, filename)
 
 if __name__ == '__main__':
-    app.run(host='0.0.0.0', port=80, debug=True)
+    app.run(host='127.0.0.1', port=5001, debug=True)

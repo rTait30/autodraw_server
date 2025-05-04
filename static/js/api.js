df05169926541772488d4d8dc29aa0dbf3ea5510
Ryan Tait <ryan.tait31@outlook.com>
2025-05-04 19:25:24 +1000

    serverc11 no /

--- static/js/api.js
+++ static/js/api.js
@@ -1,5 +1,5 @@
 export const local = false;
-export const base = local ? '' : '/copelands';
+export const base = local ? '' : '';
 
 export function loadConfigs() {
     const categories = ['shadesail', 'surgical'];

875c1d630525e06e71050086ea030c554583e430
Ryan Tait <ryan.tait31@outlook.com>
2025-06-25 17:20:02 +1000

    seperation

--- server/endpoints/api/projects/projects_api.py
+++ server/endpoints/api/projects/projects_api.py
@@ -1,6 +1,6 @@
 from flask import Blueprint, request, jsonify
 from flask_jwt_extended import jwt_required, get_jwt_identity
-from models import db, Project, ProjectAttribute, User
+from server.models import db, Project, ProjectAttribute, User
 from datetime import datetime, timezone
 
 projects_api_bp = Blueprint('projects_api', __name__)

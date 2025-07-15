e636768e19c8eceb73f766d49b5cd6e1f6e38927
Ryan Tait <ryan.tait31@outlook.com>
2025-07-16 05:59:48 +1000

    baseurl

--- react/src/utils/baseUrl.js
+++ react/src/utils/baseUrl.js
@@ -2,6 +2,8 @@ const BASE_URL = import.meta.env.DEV
   ? 'http://localhost:5001'
   : ''; // Flask + React share origin in prod
 
+BASE_URL = '';
+
 export function getBaseUrl(path) {
   return `${BASE_URL}/copelands/${path.replace(/^\/+/, '')}`;
 }
